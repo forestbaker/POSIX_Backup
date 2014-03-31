2cf98bae8d86926c6600d754f909342ef32a3172
Andrea Dari <andreadari91@gmail.com>
2014-03-31 21:48:21 +0200

    Changed the syntax of the script's invocation
    fixed few minor bugs

--- POSIX_backup.sh
+++ POSIX_backup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 ########
 #LICENSE                                                   
@@ -10,7 +10,7 @@
 # This shell script is free software: you can redistribute it and/or modify                             
 # it under the terms of the GNU General Public License as published by                                   
 # the Free Software Foundation, either version 3 of the License, or                                     
-# (at your option) any later version.                                                                   
+# any later version.                                                                   
 #                                                                                                       
 # This program is distributed in the hope that it will be useful,                                       
 # but WITHOUT ANY WARRANTY; without even the implied warranty of                                        
@@ -59,68 +59,72 @@ NOACTION='' # -n Perform a trial run with no changes made
 
 #Checks if rsync exists
 rsyncCheck() {
-    [ -x /usr/bin/rsync ] && return 0 || echo Error: rsync is not installed
-    return 1
+    [ -x /usr/bin/rsync ] && return 0 || echo "Error: rsync is not installed"
+    exit 1
 }
 
 #Checks if tar exists
 tarCheck() {
-    [ -x /bin/tar ] && return 0 || echo Error: tar is not installed
-    return 1
+    [ -x /bin/tar ] && return 0 || echo "Error: tar is not installed"
+    exit 2
 }
 
 #0 arguments -> reads files to backup from configuration file 
 #In the configuration file it is possible to use pathname expansion
 loadConfig() {
-    #Counts the number of lines
-    local NUMLINES=$(wc -l "$CONFIGDIR/$INCLUDE" | cut -d " " -f 1)
-    for i in $(seq 1 $NUMLINES); do
-	#Calling himself with a line at a time as argument
-	eval $0 $( head -n $i "$CONFIGDIR/$INCLUDE" | tail -n 1)
+    #Counts the number of lines and comments
+    local NUMLINES=$(egrep -v '(^$|^#)' "$CONFIGDIR/$INCLUDE" | wc -l)
+    for i in $(seq 1  $NUMLINES); do
+		#Calling himself with a line at a time as argument
+		eval $0 $(egrep -v '(^$|^#)' "$CONFIGDIR/$INCLUDE" | head -n $i | tail -n 1)
     done
 }
 
 #1 argument -> default action depending on ruid value
 defaultAction() {
     if [ $(id -ur) -eq 0 ]; then
-	#Root default action
-	if [ $RSYNC = 'enable' ]; then
-	    rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "/etc" "/home" "/root" "/var" "$1"
-	else
-	    #Log file is used to save output and error messagges in a file specified by user, useful in case the script is executed by Cron/Anacron (must be enabled)
-	    if [ ! $LOG = 'enable' ]; then
-		tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "/etc" "/home" "/root" "/var"
-	    else
-		tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "/etc" "/home" "/root" "/var" >> "$LOGFILE" 2>&1
-	    fi
-	    #Encryption is done if enabled
-	    [ $ENCRYPTION = 'enable' ] && gpg -evr $ENCRYPTIONRECIPIENT "$1"
-	fi
+		#Root default action
+		if [ $RSYNC = 'enable' ]; then
+	    	rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "/etc" "/home" "/root" "/var" "$1"
+		else
+	    	#Log file is used to save output and error messagges in a file specified by user, useful in case the script is executed by Cron/Anacron (must be enabled)
+	    	if [ ! $LOG = 'enable' ]; then
+				tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "/etc" "/home" "/root" "/var"
+	    	else
+				tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "/etc" "/home" "/root" "/var" >> "$LOGFILE" 2>&1
+	    	fi
+	    	#Encryption is done if enabled
+	    	[ $ENCRYPTION = 'enable' ] && gpg -evr $RECIPIENT "$1"
+		fi
     else
-	#Others default action
-	if [ $RSYNC = 'enable' ]; then
-	    rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "$HOME" "$1"
-	else
-	    if [ ! $LOG = 'enable' ];then
-	    	tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "$HOME"
-	    else
-	    	tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "$HOME" >> "$LOGFILE" 2>&1
-	    fi
-	    #Encryption is done if enabled
-	    [ $ENCRYPTION = 'enable' ] && gpg -evr $ENCRYPTIONRECIPIENT "$1"
-	fi
+		#Others default action
+		if [ $RSYNC = 'enable' ]; then
+	    	rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "$HOME" "$1"
+		else
+		    if [ ! $LOG = 'enable' ];then
+		    	tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "$HOME"
+		    else
+		    	tar -cv $COMPRESS $INCBACKUP $INCBACKUPFILE -f  "$1" "$HOME" >> "$LOGFILE" 2>&1
+		    fi
+		    #Encryption is done if enabled
+		    [ $ENCRYPTION = 'enable' ] && gpg -evr $RECIPIENT "$1"
+		fi
     fi
 }
 
 #2+ arguments
 nArguments() {
     if [ $RSYNC = 'enable' ]; then
-	rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "$@"
+		rsync -avh $NOACTION $EXCLUDE --log-file="$LOGFILE" --modify-window=1 "$@"
     else
-	#Log file is used to save output and error messagges in a file specified by user, useful in case the script is executed by Cron/Anacron (must be enabled)
-        [ ! $LOG = 'enable' ] && tar -cv $INCBACKUP $INCBACKUPFILE -f "$@" || tar -cv $INCBACKUP $INCBACKUPFILE -f "$@" >> "$LOGFILE" 2>&1
+		#Log file is used to save output and error messagges in a file specified by user, useful in case the script is executed by Cron/Anacron (must be enabled)
+        if [ ! $LOG = 'enable' ] ; then
+        	eval tar -cv $INCBACKUP $INCBACKUPFILE -f $(printf '"%s"\n' "$@" | tac) 
+        else
+        	eval tar -cv $INCBACKUP $INCBACKUPFILE -f $(printf '"%s"\n' "$@" | tac) >> "$LOGFILE" 2>&1	
+        fi
         #Encryption is done if enabled
-        [ $ENCRYPTION = 'enable' ] && gpg -evr "$ENCRYPTIONRECIPIENT" "$1"
+        [ $ENCRYPTION = 'enable' ] && gpg -evr "$RECIPIENT" "$1"
     fi
 }
 
@@ -128,81 +132,53 @@ nArguments() {
 #SCRIPT START
 #############
 
-#Commandline options are parsed by getopts command
- while getopts :trce:g:l:n OPTION; do
+#Parsing command line options
+while getopts ':trce:g:l:n' OPTION; do
 	case $OPTION in
-	    t)  
-                #Tar
-		tarCheck || exit 1
-		[ $RSYNC = 'enable' ] && echo "Options -r and -t cannot be used together " && exit 3
-		TAR='enable'
-		;;
+	    t)	
+	    	tarCheck
+	    	[ $RSYNC = 'enable' ] && echo "Error: options -r and -t cannot be used together " && exit 3
+	    	TAR='enable' ;;
 	    r)  
-                #Rsync
-		rsyncCheck || exit 2
-		[ $TAR = 'enable' ] && echo "Options -t and -r cannot be used together " && exit 3
-		RSYNC='enable'
-		;;
-       	    c)  
-		#Compression
-		COMPRESS='-z'
-		;;
+	    	rsyncCheck
+	    	[ $TAR = 'enable' ] && echo "Error: options -t and -r cannot be used together " && exit 3
+	    	RSYNC='enable' ;;
+		c)  
+			COMPRESS='-z' ;;
 	    e)  
-		#Encryption
-		ENCRYPTION='enable'
-		ENCRYPTIONRECIPIENT="$OPTARG"
-		shift
-		;;
-	    g) 
-		#Incremental backup
-		INCBACKUP='-g'
-		INCBACKUPFILE="$OPTARG" 
-		shift
-		;;
-	    l)
-		#Log file destination
-		LOG='enable'
-		LOGFILE="$OPTARG"
-		shift
-		;;
+	    	ENCRYPTION='enable' ; RECIPIENT="$OPTARG" ;;
+	    g)  
+	    	INCBACKUP='-g' ; INCBACKUPFILE="$OPTARG" ;; 
+	    l)  
+	    	LOG='enable' ; LOGFILE="$OPTARG" ;;
 	    n)  
-		#Trial run
-		NOACTION='-n'
-		;;
+	    	NOACTION='-n' ;;
+		#Invalid option
 	    \?) 
-                #Invalid option
-		echo "Invalid option: -$OPTARG"
-		exit 4
-		;;
-	    :) 
-                #Missing argument
-		echo "Option -$OPTARG requires an argument"
-		exit 5
-		;;
+	    	echo "Invalid option: -$OPTARG" ; exit 4 ;;
+		#Missing argument	    
+		:)  
+			echo "Option -$OPTARG requires an argument" ; exit 5 ;;
 	esac
-	shift
-    done
+done
+shift $(($OPTIND - 1 ))
 #Another error control
 [ $TAR = 'disable' -a $RSYNC = 'disable' -a $# -gt 0 ] && echo "Error: -t (tar) or -r (rsync) option must be specified" && exit 6
 #Checks the number of arguments and calls the appropriate functions
 case $# in
-    0)
 	#Reads the configuration from $INCLUDE if exists and if readable
-	if [ -r "$CONFIGDIR/$INCLUDE" ]; then
-	    loadConfig
-	else
-	    echo "Error: $CONFIGDIR/$INCLUDE must exists" && exit 7
-        fi
-	;;
-    1)
+    0) 
+    	if [ -r "$CONFIGDIR/$INCLUDE" ] ; then
+    		loadConfig 
+    	else 
+    		echo "Error: $CONFIGDIR/$INCLUDE must exists" ; exit 7
+    	fi ;;
 	#Default action depending on ruid value
-        defaultAction "$1"
-	;;
-    *)
+    1) 
+    	defaultAction "$1" ;;
 	#Like using tar or rysnc directly
-	nArguments "$@"
-	;;
+    *) 
+    	nArguments "$@" ;;
 esac
-
 #Script exit
 exit 0
